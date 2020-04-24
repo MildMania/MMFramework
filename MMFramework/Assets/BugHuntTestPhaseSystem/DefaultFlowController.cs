ab65937a55dbf5ae3fd6afb8e403d7b612e047f7
Omer AVCI <o@mildmania.com>
2020-04-24 21:48:03 +0300

    default flow controller update

--- MMFramework/Assets/BugHuntTestPhaseSystem/DefaultFlowController.cs
+++ MMFramework/Assets/BugHuntTestPhaseSystem/DefaultFlowController.cs
@@ -2,10 +2,12 @@
 {
     protected override PhaseBaseNode CreateRootNode()
     {
+        HuntPhase huntPhase = new HuntPhase();
+
         return new PhaseSerialComposition
         (
             new InitialPhase(), // Drone waits, main menu opens up, character enters to the scene and count down starts
-            new HuntPhase(), // Gameplay starts
+            huntPhase, // Gameplay starts
             new HuntCatchPhase // Mini game
             (
                 new PhaseSerialComposition // 1 of 2 mini game result
@@ -15,13 +17,13 @@
                     (
                         new LevelWinPhase(), // Game finish success, char makes another dance, dragon eats apple, level end success UI, success music plays
                         new LevelFailPhase(), // Game finish fail, char makes sad dance, dragon kills us, level end fail UI, fail music plays
-                        new GotoHuntPhase(new HuntPhase()) // game is not ready to finish (ie. not all bugs are catched, not all objecives are completed etc)
+                        new GotoHuntPhase(huntPhase) // game is not ready to finish (ie. not all bugs are catched, not all objecives are completed etc)
                     )
                 ),
                 new PhaseSerialComposition // other mini game result
                 (
                     new CatchFailPhase(), // char fails to catch, or gets away from bug
-                    new GotoHuntPhase(new HuntPhase()) // returns to hunt phase
+                    new GotoHuntPhase(huntPhase) // returns to hunt phase
                 )
             )
         );
